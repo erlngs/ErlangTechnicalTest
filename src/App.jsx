a8b1ab5b504f905c4511b52484e1d85e0afb1bde
Erlang Sinatrya Putra Yusya <erlangs12@gmail.com>
2026-06-04 23:52:20 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import DashboardLayout from './components/layout/DashboardLayout'
+
+function App() {
+  return (
+    <DashboardLayout>
+      <p>Hello Dashboard</p>
+    </DashboardLayout>
+  )
+}
+
+export default App
